70e9ab3a2c539c635eca8bd985f861ffff1d98c0
Jawad <chaptrhouse@gmail.com>
2026-08-30 22:24:25 -0500

    chore(release): v1.5.0

--- packages/changelog/src/entries/index.ts
+++ packages/changelog/src/entries/index.ts
@@ -1,5 +1,6 @@
 import type { ComponentType } from 'react'
 import Entry_1_3_0 from './1.3.0.mdx'
+import Entry_1_5_0 from './1.5.0.mdx'
 
 export interface ChangelogEntry {
   version: string
@@ -8,6 +9,7 @@ export interface ChangelogEntry {
 }
 
 export const entries: ChangelogEntry[] = [
+  { version: '1.5.0', date: '2026-08-30', body: Entry_1_5_0 },
   { version: '1.3.0', date: '2026-08-30', body: Entry_1_3_0 }
 ]
 
